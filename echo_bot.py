a95bcb4f170bf0d74ec6b919778f18bfcb342738
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-15 18:49:05 -0500

    Use a fine tuned model

--- echo_bot.py
+++ echo_bot.py
@@ -10,17 +10,17 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def echo(message, history):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
+        model="tunedModels/sarcastic-g1851wnu1i60",                
+        # model="gemini-2.0-flash-001",
         contents=message,
     )
-
     return response.text
 
 
 iface = gr.ChatInterface(
     fn=echo,
     textbox=gr.Textbox(placeholder="Type your message here..."),
-    title="My Echo Bot"
+    title="My helpful Bot"
 )
 
 iface.launch()
