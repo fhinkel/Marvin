2cfbb6cb5eb383de49dfabb9307bec673c1abdd7
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-14 20:46:43 -0500

    Add Gemini to chat

--- echo_bot.py
+++ echo_bot.py
@@ -1,7 +1,21 @@
 import gradio as gr
+from google import genai
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def echo(message, history):
-    return message  # Just return the user's message
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=message,
+    )
+
+    return response.text
+
 
 iface = gr.ChatInterface(
     fn=echo,
