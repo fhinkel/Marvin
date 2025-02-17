cfccf1c8b9ac2506176ef0517f6b8f3d6f942bc1
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-16 20:56:08 -0500

    use type message to get rid of warning

--- echo_bot.py
+++ echo_bot.py
@@ -2,14 +2,12 @@ import gradio as gr
 from google import genai
 from google.genai import types
 
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
 
 def echo(message, history):
     response = client.models.generate_content(
@@ -30,7 +28,8 @@ def echo(message, history):
 iface = gr.ChatInterface(
     fn=echo,
     textbox=gr.Textbox(placeholder="Type your message here..."),
-    title="My helpful Bot"
+    title="Marvin the Paranoid Android",
+    type="messages"
 )
 
 iface.launch()
