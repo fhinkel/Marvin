e37d88c51a517c00eda83c7a7b08f9372dbac807
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-16 19:49:19 -0500

    Handle safety issues somewhat gracefully

--- echo_bot.py
+++ echo_bot.py
@@ -1,5 +1,7 @@
 import gradio as gr
 from google import genai
+from google.genai import types
+
 
 import os
 from dotenv import load_dotenv
@@ -8,12 +10,20 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+
 def echo(message, history):
     response = client.models.generate_content(
-        model="tunedModels/sarcastic-g1851wnu1i60",                
+        model="tunedModels/sarcastic-g1851wnu1i60",
         # model="gemini-2.0-flash-001",
         contents=message,
     )
+
+    if response.text is None:
+        print(response.candidates[0].finish_reason)
+        if response.candidates[0].finish_reason is types.FinishReason.SAFETY:
+            print("It's a safety issue")
+            return "Safety violation. Please ask something else"
+
     return response.text
 
 
@@ -24,3 +34,6 @@ iface = gr.ChatInterface(
 )
 
 iface.launch()
+
+# echo("what color are oranges?", None)
+
