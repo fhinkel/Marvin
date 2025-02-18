011aff2592c77af60e8a211fd77a4af674b35778
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-17 23:03:09 -0500

    Use a default model, not the fine tuned one

--- marvin_bot.py
+++ marvin_bot.py
@@ -4,14 +4,12 @@ from google.genai import types
 
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-chat = client.chats.create(model="tunedModels/sarcastic-g1851wnu1i60")
-# model="gemini-2.0-flash-001",
-
+# chat = client.chats.create(model="tunedModels/sarcastic-g1851wnu1i60")
+chat = client.chats.create(model="gemini-2.0-flash-001")
 
 def echo(message, history):
     response = chat.send_message(message)
